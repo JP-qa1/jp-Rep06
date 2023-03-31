0dcb4275ce1c73b35fb79e917a5430b2ab175350
JP-qa1 <jsp_us@yahoo.com>
2023-03-30 20:13:09 -0400

    Test Commit 03302023

---
+++ JPMavenGitHubJenkinsTest4/src/test/java/JPMavenGitHubJenkinsTest4pkg/JPTest4.java
@@ -0,0 +1,17 @@
+package JPMavenGitHubJenkinsTest4pkg;
+
+import org.testng.annotations.Test;
+
+public class JPTest4
+
+{
+	@Test
+	(groups= {"SmokeTest"})
+	
+	public void method4()
+	{
+		System.out.println("Method4 is running");
+		System.out.println("----");
+
+	}
+}
---
+++ JPMavenGitHubJenkinsTest4/src/test/java/JPMavenGitHubJenkinsTest4pkg/package-info.java
@@ -0,0 +1,1 @@
+package JPMavenGitHubJenkinsTest4pkg;
